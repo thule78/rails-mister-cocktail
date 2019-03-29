ca8a50d0108143c8ef0ee3dac277a0e009202423
thule <lunthu@gmail.com>
2019-03-29 13:57:19 +0900

    change- without photo

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,41 @@
+class CocktailsController < ApplicationController
+
+  before_action :find_cocktail, only: [:show, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @ingredients = Ingredient.order("name asc")
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "cocktails#index"
+  resources :cocktails do
+    resources :doses, only: [:index, :show, :create, :new]
+    end
+    resources :doses, only: [:index, :show, :edit, :update, :destroy]
 end
